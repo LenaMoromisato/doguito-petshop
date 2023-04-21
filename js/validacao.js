645b8cda56babbaf1c09a741233e49644ebab7bb
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-21 18:47:54 -0300

    customizando as mensagens de erro

--- js/validacao.js
+++ js/validacao.js
@@ -7,10 +7,19 @@ export function valida(input) {
 
     if(input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
     } else {
         input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
     }
 }
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
 
 const mensagensDeErro = {
     nome: {
@@ -21,7 +30,7 @@ const mensagensDeErro = {
         typeMismatch: 'O email digitado não é válido.'
     },
     senha: {
-        valuemissing: 'O campo de senha não pode estar vazio.',
+        valueMissing: 'O campo de senha não pode estar vazio.',
         patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
     },
     dataNascimento: {
@@ -32,6 +41,17 @@ const mensagensDeErro = {
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = '';
+
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro];
+        }
+    })
+    return mensagem;
 }
 
 function validaDataNascimento(input) {
